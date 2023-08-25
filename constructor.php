e4175900117fe0c9eb0b97608ede7d42162cf082
Ajay Mahato <120182531+Summerlove9988@users.noreply.github.com>
2023-08-25 15:02:10 +0545

    Update constructor.php

--- constructor.php
+++ constructor.php
@@ -1,23 +1,26 @@
 <?php
 
-    class car{
-        public $name;
-        public $color;
+class car
+{
+    public $name;
+    public $color;
 
-        function __construct($name){
-            $this -> name = $name;
-        }
-        
-        function get_name(){
-            echo $this -> name;
-        }
+    function __construct($name)
+    {
+        $this->name = $name;
     }
 
-    $audi = new car('Audi');
-    $audi -> color = 'Navy Blue';
+    function get_name()
+    {
+        echo $this->name;
+    }
+}
 
-    $audi -> get_name();
-    echo '<br>';
-    echo $audi -> color;
+$audi = new car('Audi');
+$audi->color = 'Navy Blue';
+
+$audi->get_name();
+echo '<br>';
+echo $audi->color;
 
 ?>
