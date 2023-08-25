055c276544a207252c2f3b3d5290dd8d15728b35
Ajay Mahato <120182531+Summerlove9988@users.noreply.github.com>
2023-08-25 15:02:38 +0545

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>PHP OOP</title>
 </head>
 <style>
-    a{
+    a {
         text-decoration: none;
     }
 </style>
@@ -16,11 +16,13 @@
 
     <h2>PHP OOP Projects </h2>
 
-        <ol>
-            <li><a href="rectangle-oop.php">Area and Perimeter of Rectangle</a></li>
-            <li><a href="construct.php">Example of PHP Constructor</a></li>
-            <li><a href="schooldemo.php">Example of abstract classes and access modifiers</a></li>
-        </ol>
+    <ol>
+        <li><a href="rectangle-oop.php">Area and Perimeter of Rectangle</a></li>
+        <li><a href="circle.php">Area and Circumference of Circle</a></li>
+        <li><a href="triangle-rectangle.php">Area of Rectangle and Triangle using Abstract Method</a></li>
+        <li><a href="construct.php">Example of PHP Constructor</a></li>
+        <li><a href="schooldemo.php">Example of abstract classes and access modifiers</a></li>
+    </ol>
 
 </body>
 
